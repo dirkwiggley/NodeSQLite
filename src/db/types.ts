5a8434948fe727f70104a3930e78691917eec247
Dirk Wiggley <dirkwiggley@gmail.com>
2024-01-16 13:03:12 -0800

    Backtracking
    Backtracking

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,7 +1,14 @@
 export interface UserInterface {
   id: number;
   login: string;
+  password: string;
   nickname: string;
+  email: string;
+  roles: string;
+  locale: string;
+  active: number;
+  resetpwd: number;
+  refreshtoken: string;
 }
 
 export interface VideoInterface {
@@ -22,3 +29,18 @@ export interface VideoInListInterface {
   list_id: number;
   video_id: number;
 }
+
+export interface TokenInterface {
+  exp: number;
+  iat: number;
+  isAdmin: boolean;
+  login: string;
+  timestamp: number;
+  user_id: number;
+}
+
+// TODO: flesh this out
+export const objectIsDecodedToken = (obj: unknown): obj is TokenInterface => {
+  const test: any = obj;
+  return test.timestamp !== null && test.timestamp !== undefined;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
+import videoRoute from "./routes/videos.js"
+import videoListRoute from './routes/videoList.js'
+import videoInListRoute from './routes/videoInList.js';
 import express from "express"
 import * as dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+import authRoute from "./routes/auth.js"
 import userRoute from "./routes/users.js"
-import videoRoute from "./routes/videos.js"
-import videoListRoute from './routes/videoList.js'
-import videoInListRoute from './routes/videoInList.js';
+import roleRoute from "./routes/roles.js"
 import dbUtilsRoute from "./routes/dbutils.js"
+import testRoute from "./routes/test.js"
 
 const app = express();
 if (process.env.DEBUG !== 'true') {
@@ -33,12 +36,15 @@ console.debug(process.env.ACCESS_KEY)
 app.use(express.json())
 app.use(cookieParser())
 
+app.use("/auth", authRoute)
 app.use("/users", userRoute)
+app.use("/roles", roleRoute)
+app.use("/dbutils", dbUtilsRoute)
 app.use("/videos", videoRoute)
 app.use("/video_list", videoListRoute)
 app.use("/video_in_list", videoInListRoute)
-app.use("/dbutils", dbUtilsRoute)
-app.use("/", userRoute)
+app.use("/test", testRoute)
+app.use("/", testRoute)
 
 // Error handler
 app.use((err,req,res,next)=>{
